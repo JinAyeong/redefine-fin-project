cc4c4fc21c1e7e873723a1b8e26177a44bb3b657
Jin Ayeong <ay8160@daum.net>
2024-05-16 21:46:07 +0900

    article_serializer_views

--- back/articles/views.py
+++ back/articles/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ArticleListSerializer, ArticleDetailSerializer, CommentSerializer
+from .models import Article, Comment
 
-# Create your views here.
+
+# 게시글 리스트 조회 / 게시글 작성
+@api_view(['GET', 'POST'])
+def article_list(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = ArticleDetailSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+
+# 게시글 상세 조회 / 수정 / 삭제
+api_view(['GET', 'PUT', 'DELETE'])
+def article_detail(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+
+    if request.method == 'GET':
+        serializer = ArticleDetailSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleDetailSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+# 댓글 리스트 조회 / 생성
+@api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+def comment_list(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    comments = Comment.objects.all()
+    
+    if request.method == 'GET':
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(article=article)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# 댓글 상세 조회 / 수정 / 삭제
+@api_view(['GET', 'DELETE', 'PUT'])
+def comment_detail(request, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
