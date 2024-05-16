6afd90c1825f36c3f5d252830861774a88cbacaf
Jin AYeong <ay8160@daum.net>
2024-05-16 17:32:53 +0900

    0516-first

---
+++ back/accounts/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from allauth.account.adapter import DefaultAccountAdapter
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=10)
+    email = models.EmailField(max_length=30)
+    profile_img = models.ImageField(blank=True, null=True)
+    age = models.IntegerField()
+    money = models.IntegerField(blank=True, null=True)
+    salary = models.IntegerField(blank=True, null=True)
+    # 가입한 상품 목록 리스트를 ,로 구분된 문자열로 저장함
+    financial_products = models.TextField(blank=True, null=True)
+
+    # superuser fields
+    is_superuser = models.BooleanField(default=False)
+
+ 
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def save_user(self, request, user, form, commit=True):
+        """
+        Saves a new `User` instance using information provided in the
+        signup form.
+        """
+        from allauth.account.utils import user_email, user_field, user_username
+        
+        data = form.cleaned_data
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        email = data.get("email")
+        username = data.get("username")
+        # name 필드를 추가
+        name = data.get("name")
+
+        user_email(user, email)
+        user_username(user, username)
+        if first_name:
+            user_field(user, "first_name", first_name)
+        if last_name:
+            user_field(user, "last_name", last_name)
+        if name:
+            user_field(user, "name", name)
+        if "password1" in data:
+            user.set_password(data["password1"])
+        else:
+            user.set_unusable_password()
+        self.populate_username(request, user)
+        if commit:
+            # Ability not to commit makes it easier to derive from
+            # this adapter by adding
+            user.save()
+        return user
