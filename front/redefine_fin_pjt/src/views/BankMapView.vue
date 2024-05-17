75885ec9959ad336a3180dcaad6e43d44119cca1
홍수인 <ghdtndls2000@naver.com>
2024-05-17 22:49:45 +0900

    exchange view

---
+++ front/redefine_fin_pjt/src/views/BankMapView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Bank Map Page</h1>
+        <div id="map" style="width:500px;height:400px;"></div>
+    </div>
+</template>
+
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=0f8d9fd188fed84be55db898f5dcd227&libraries=services,clusterer,drawing"></script>
+<script setup>
+    // 1766462dae4b78aff86eb4373ecb1bc4
+
+</script>
+
+<style scoped>
+
+</style>
