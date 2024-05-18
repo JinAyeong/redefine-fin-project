0fbbcca9b1360428355b4508b8e57561476271b1
홍수인 <ghdtndls2000@naver.com>
2024-05-18 20:26:30 +0900

    map-complete

--- front/redefine_fin_pjt/src/App.vue
+++ front/redefine_fin_pjt/src/App.vue
@@ -8,11 +8,8 @@
       <RouterLink :to="{name : 'article'}">게시판</RouterLink>
       <RouterLink :to="{name : 'signup'}">회원가입</RouterLink>
       <RouterLink :to="{name : 'home'}">메인페이지</RouterLink>
-<<<<<<< HEAD
       <RouterLink :to="{name : 'bankmap'}">지도</RouterLink>
-=======
       <p v-if="profilestore.isLogin">환영해~</p>
->>>>>>> fcfc8bc056adb64813e42abbef1e95cbddafae02
     </nav>
   </div>
   <RouterView />
