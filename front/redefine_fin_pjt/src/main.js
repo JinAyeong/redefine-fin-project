7809c077b13aed007b6a377622f07e7fcf9d0b41
Jin Ayeong <ay8160@daum.net>
2024-05-19 16:06:04 +0900

    profile_50%

--- front/redefine_fin_pjt/src/main.js
+++ front/redefine_fin_pjt/src/main.js
@@ -1,14 +1,13 @@
 // src/main.js
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import App from './App.vue'
+import router from './router'
 
 // Pinia 생성 및 플러그인 사용
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
-
 const app = createApp(App)
 
 app.use(pinia)
